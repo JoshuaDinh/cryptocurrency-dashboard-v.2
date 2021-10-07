bb5f30dffe4f7b494aa91d1787e94b35a4a055d8
Joshua Dinh <joshuahungdinh@gmail.com>
2021-10-07 14:34:16 -0700

    added functionality to make multiple git requests based on a selected coin from input. These requests will contain information on the selected coin and their pricing over periods of different times. 1hr, 1day, 7days, 14days, 30days,

--- src/Sections/Discover/Discover.js
+++ src/Sections/Discover/Discover.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./discover.css";
+import Search from "Components/Search/Search";
 import LineChart from "Components/LineChart/LineChart";
 import DiscoverCard from "Components/DiscoverCard/DiscoverCard";
 import DiscoverHeader from "Components/DiscoverHeader/DiscoverHeader";
 
 const Discover = () => {
+  const [selectedCoin, setSelectedCoin] = useState("");
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    try {
+      async function fetchData() {
+        const oneDayResponse = await fetch(
+          `https://api.coingecko.com/api/v3/coins/${selectedCoin}/market_chart?vs_currency=usd&days=1`
+        );
+        const responseOne = oneDayResponse.json();
+        const oneWeekResponse = await fetch(
+          `https://api.coingecko.com/api/v3/coins/${selectedCoin}/market_chart?vs_currency=usd&days=7`
+        );
+        const responseTwo = oneWeekResponse.json();
+        const twoWeekResponse = await fetch(
+          `https://api.coingecko.com/api/v3/coins/${selectedCoin}/market_chart?vs_currency=usd&days=14`
+        );
+        const responseThree = twoWeekResponse.json();
+        const oneMonthResponse = await fetch(
+          `https://api.coingecko.com/api/v3/coins/${selectedCoin}/market_chart?vs_currency=usd&days=30`
+        );
+        const responseFour = oneMonthResponse.json();
+        const data = await Promise.all([
+          responseOne,
+          responseTwo,
+          responseThree,
+          responseFour,
+        ]);
+        setChartData(data);
+      }
+      if (selectedCoin) {
+        fetchData();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [selectedCoin]);
+
   return (
     <section className="discover">
+      <Search setSelectedCoin={setSelectedCoin} />
       <DiscoverHeader />
       <div className="discover-details">
         <LineChart />
