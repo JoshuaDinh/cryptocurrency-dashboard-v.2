fdc3cf5fd89a27d36b3d743c0bcf5b84452afb69
Joshua Dinh <joshuahungdinh@gmail.com>
2021-10-08 07:26:22 -0700

    added files and markup

--- src/Sections/MarketCap/MarketCap.js
+++ src/Sections/MarketCap/MarketCap.js
@@ -1,18 +1,11 @@
-import Card from "Components/Card/Card";
-import Table from "Components/Table/Table";
+import MarketCapTopFive from "Components/MarketCapTopFive/MarketCapTopFive";
 import React from "react";
 import "./marketCap.css";
 
-const MarketCap = () => {
+const MarketCap = ({ coinList }) => {
   return (
     <div className="market-cap">
-      <div className="mc-card-container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <MarketCapTopFive coinList={coinList} />
     </div>
   );
 };
