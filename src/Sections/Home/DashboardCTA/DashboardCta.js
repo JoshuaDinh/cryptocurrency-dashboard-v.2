48402184251d57c62c7e9645b493b64195ecc414
Joshua Dinh <joshuahungdinh@gmail.com>
2021-10-14 21:58:50 -0700

    added files:

---
+++ src/Sections/Home/DashboardCTA/DashboardCta.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./dashboardCta.css";
+
+const DashboardCta = ({ coinList }) => {
+  return <div className="dashboard-cta">cta</div>;
+};
+
+export default DashboardCta;
